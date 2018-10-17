7fbcd6dd1e50ed943e303a991afe63f7852c36df
Jitin Khemwal <jitin.mobile.dev@gmail.com>
2018-10-17 18:42:53 +0530

    Add Design Library for custom views

---
+++ design/src/main/java/com/design/jackdaniels/JDEditText.java
@@ -0,0 +1,49 @@
+package com.design.jackdaniels;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Typeface;
+import android.support.v7.widget.AppCompatEditText;
+import android.support.v7.widget.AppCompatTextView;
+import android.util.AttributeSet;
+
+
+public class JDEditText extends AppCompatEditText {
+
+
+    public JDEditText(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(attrs);
+    }
+
+    public JDEditText(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(attrs);
+    }
+
+    public JDEditText(Context context) {
+        super(context);
+        init(null);
+    }
+
+
+    private void init(AttributeSet attrs) {
+        if (attrs != null) {
+            TypedArray a = getContext().obtainStyledAttributes(attrs, R.styleable.CustomView);
+
+            String fontName = a.getString(R.styleable.CustomView_font_name);
+
+            try {
+                if (fontName != null) {
+                    Typeface myTypeface = Typeface.createFromAsset(getContext().getAssets(), "fonts/" + fontName);
+                    setTypeface(myTypeface);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            a.recycle();
+        }
+    }
+
+}
---
+++ design/src/main/java/com/design/jackdaniels/JDTextView.java
@@ -0,0 +1,52 @@
+package com.design.jackdaniels;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Typeface;
+import android.os.Build;
+import android.support.v7.widget.AppCompatButton;
+import android.support.v7.widget.AppCompatTextView;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+
+public class JDTextView extends AppCompatTextView {
+
+
+    public JDTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(attrs);
+    }
+
+    public JDTextView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(attrs);
+    }
+
+    public JDTextView(Context context) {
+        super(context);
+        init(null);
+    }
+
+
+    private void init(AttributeSet attrs) {
+        if (attrs != null) {
+            TypedArray a = getContext().obtainStyledAttributes(attrs, R.styleable.CustomView);
+
+            String fontName = a.getString(R.styleable.CustomView_font_name);
+
+            try {
+                if (fontName != null) {
+                    Typeface myTypeface = Typeface.createFromAsset(getContext().getAssets(), "fonts/" + fontName);
+                    setTypeface(myTypeface);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            a.recycle();
+        }
+    }
+
+}
